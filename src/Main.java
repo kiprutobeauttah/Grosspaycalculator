9450ffe13573c1f08b2422a38b2a6ef084a0f483
kiprutobeauttah <kiprutobeauttah@gmail.com>
2024-10-14 11:24:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+class Payroll{
+    public static void main(String[] args){
+        String name;
+        int hour;
+        Double payRate;
+        Double grossPay;
+        Scanner keyboard = new Scanner(System.in);
+        //get the user name
+        System.out.print("Enter your name");
+        name = keyboard.nextLine();
+        //get hours
+        System.out.print("how many hours did you work ?");
+        hour = keyboard.nextInt();
+        //payratye
+        System.out.print("what is your hourly payrate?");
+        payRate =keyboard .nextDouble();
+        // gross pay
+        grossPay = hour * payRate;
+        System.out.println("Hello " +name);
+        System.out.println("your grosspay is $"+ grossPay);
     }
 }
